33dbf51b1a1ecf6006a71c6b2f56b7782b75befa
Aleksei Baliuk <aleksred.b@gmail.com>
2022-08-15 23:49:54 +0300

    finish-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+// const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
+const PLAY_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  const stringifyData = JSON.stringify(data);
+  localStorage.setItem(PLAY_TIME, stringifyData);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function resumePlay() {
+  if (JSON.parse(localStorage.getItem(PLAY_TIME)) === null) return;
+
+  const currentTime = JSON.parse(localStorage.getItem(PLAY_TIME))['seconds'];
+
+  player
+    .setCurrentTime(currentTime)
+    .then(function (seconds) {})
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          break;
+
+        default:
+          break;
+      }
+    });
+}
+
+resumePlay();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+populateForm();
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+
+  //   console.log(formData.email);
+
+  if (!formData.email || !formData.message) {
+    alert('Fill all fields');
+    populateForm();
+    return;
+  }
+
+  if (
+    Object.keys(formData).length !== 0 &&
+    JSON.parse(localStorage.getItem(STORAGE_KEY)) !== null
+  ) {
+    console.log(formData);
+  }
+
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData) {
+    Object.entries(savedData).forEach(([name, value]) => {
+      refs.form[name].value = value;
+      formData[name] = value;
+    });
+  }
+}
